db78aacfd624ccab1898e8bc24f193d27deef03e
Timo M. Staudinger <staudinger.timo@gmail.com>
2018-03-10 22:48:02 -0500

    Make icon picker searchable

--- packages/webui/src/components/IconPicker.js
+++ packages/webui/src/components/IconPicker.js
@@ -11,6 +11,9 @@ import CloseIcon from 'material-ui-icons/Close'
 import Slide from 'material-ui/transitions/Slide'
 import Tooltip from 'material-ui/Tooltip'
 import {allIcons} from './IconLibrary'
+import {Paper, InputAdornment, TextField} from 'material-ui'
+import SearchIcon from 'material-ui-icons/Search'
+import {grey} from 'material-ui/colors'
 
 const styles = {
   appBar: {
@@ -30,6 +33,9 @@ const styles = {
   grid: {
     maxWidth: 1000,
     margin: 'auto'
+  },
+  searchContainer: {
+    margin: 16
   }
 }
 
@@ -37,26 +43,85 @@ function Transition(props) {
   return <Slide direction="up" {...props} />
 }
 
+class IconPickerButton extends React.PureComponent {
+  render() {
+    const {icon, classes} = this.props
+
+    return (
+      <Grid item xs={2} className={classes.iconContainer}>
+        <Tooltip title={icon.name}>
+          <Button>
+            <icon.component className={classes.icon} fontSize />
+          </Button>
+        </Tooltip>
+      </Grid>
+    )
+  }
+}
+
+const defaultState = {
+  rows: 12,
+  filter: ''
+}
+
 class IconPicker extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      rows: 12
+    this.onEnter = this.onEnter.bind(this)
+    this.onExit = this.onExit.bind(this)
+    this.onScroll = this.onScroll.bind(this)
+
+    this.state = defaultState
+  }
+
+  onEnter() {
+    this.setState(defaultState)
+
+    this.dialogElement = document.getElementById('iconPickerDialog')
+    this.dialogElement.addEventListener('scroll', this.onScroll)
+  }
+
+  onScroll() {
+    const bottomOffset = 300
+
+    const showMore =
+      this.dialogElement.scrollHeight -
+        this.dialogElement.scrollTop -
+        bottomOffset <
+      this.dialogElement.clientHeight
+    if (showMore) {
+      this.setState(state => ({rows: state.rows + 4}))
     }
+  }
+
+  onExit() {
+    this.dialogElement.removeEventListener('scroll', this.onScroll)
   }
 
   render() {
     const {onClose, classes, isOpen} = this.props
 
+    const filteredIcons =
+      this.state.filter && this.state.filter.length
+        ? allIcons.filter(i =>
+            i.name.toLowerCase().includes(this.state.filter.toLowerCase())
+          )
+        : allIcons
+    const slicedIcons = filteredIcons.slice(0, this.state.rows * 6)
+
     return (
       <Dialog
+        PaperProps={{id: 'iconPickerDialog'}}
+        ref={ref => (this.dialogRef = ref)}
         fullScreen
         open={isOpen}
         onClose={onClose}
+        onEnter={this.onEnter}
+        onExit={this.onExit}
         transition={Transition}
       >
-        <AppBar className={classes.appBar}>
+        <AppBar className={classes.appBar} style={{position: 'sticky'}}>
           <Toolbar>
             <IconButton color="inherit" onClick={onClose} aria-label="Close">
               <CloseIcon />
@@ -70,15 +135,39 @@ class IconPicker extends React.Component {
             </Typography>
           </Toolbar>
         </AppBar>
+
+        <Paper
+          style={{
+            ...styles.root,
+            position: 'sticky',
+            top: 64,
+            zIndex: 100
+          }}
+        >
+          <div style={styles.searchContainer}>
+            <TextField
+              onChange={e => this.setState({filter: e.target.value})}
+              value={this.state.filter}
+              fullWidth
+              style={styles.input}
+              placeholder="Search"
+              InputProps={{
+                disableUnderline: true,
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton>
+                      <SearchIcon style={{color: grey[500]}} />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+          </div>
+        </Paper>
+
         <Grid className={classes.grid} container spacing={8}>
-          {allIcons.slice(0, this.state.rows * 6).map(icon => (
-            <Grid key={icon.name} item xs={2} className={classes.iconContainer}>
-              <Tooltip title={icon.name}>
-                <Button>
-                  <icon.component className={classes.icon} fontSize />
-                </Button>
-              </Tooltip>
-            </Grid>
+          {slicedIcons.map(i => (
+            <IconPickerButton icon={i} classes={classes} key={i.name} />
           ))}
         </Grid>
       </Dialog>
